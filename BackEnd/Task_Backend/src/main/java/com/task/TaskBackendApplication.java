4f773ef8b64a2eb1fecb055b5ef823bf20ed8534
PARIMAL BISWAS <biswasparimal01@gmail.com>
2023-03-28 14:26:53 +0530

    .

--- BackEnd/Task_Backend/src/main/java/com/task/TaskBackendApplication.java
+++ BackEnd/Task_Backend/src/main/java/com/task/TaskBackendApplication.java
@@ -4,9 +4,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TaskBackendApplication {
+@OpenAPIDefinition(info = @Info(title = "REST API", version = "1.1"), security =
+{ @SecurityRequirement(name = "basicAuth"), @SecurityRequirement(name = "bearerToken") }, servers =
+{ @Server(url = "/", description = "Default Server URL") })
+@SecuritySchemes(
+{ @SecurityScheme(name = "basicAuth", type = SecuritySchemeType.HTTP, scheme = "basic"),
+		@SecurityScheme(name = "bearerToken", type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT") })
 
-	public static void main(String[] args) {
+public class TaskBackendApplication
+{
+
+	public static void main(String[] args)
+	{
 		SpringApplication.run(TaskBackendApplication.class, args);
 	}
 
